d866f3f65a4a54c47769d6cc6e04620d1eee9dd3
trhinehart <troy.rhinehart@gmail.com>
2016-07-15 16:25:57 -0700

    production flags

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,11 @@ module.exports = {
       output: {
         comments: false
       }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
     })
   ]
 };
